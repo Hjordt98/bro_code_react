574b48c0920c009f050cb8906e21ca4789d50b7b
Hjordt98 <andreas.hjordt@gmail.com>
2024-08-08 13:01:44 +0200

    ch 9 onChange event handler done

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -5,6 +5,8 @@ function MyComponent(){
     const [name, setName] = useState("Guest");
     const [quantity, setQuantity] = useState(1);
     const [comment, setComment] = useState("")
+    const [payment, setPayment] = useState("visa")
+    const [shipping, setShipping] = useState("Delivery");
 
     function handleNameChange(event){
         setName(event.target.value);
@@ -18,6 +20,14 @@ function MyComponent(){
         setComment(event.target.value);
     }
 
+    function handlePaymentChange(event){
+        setPayment(event.target.value);
+    }
+    
+    function handleShippingChange(event){
+        setShipping(event.target.value);
+    }
+
     return(
             <div>
                 <input value={name} onChange={handleNameChange}/>
@@ -29,6 +39,29 @@ function MyComponent(){
                 <textarea value={comment} onChange={handleCommentChange}
                 placeholder="Enter delivery instructions"/>
                 <p>comment: {comment}</p>
+
+                <select value={payment} onChange={handlePaymentChange}>
+                        <option value="">select and value</option>
+                        <option value={"visa"}>visa</option>
+                        <option value={"mastercard"}>mastercard</option>
+                        <option value={"giftcard"}>giftcard</option>
+                </select>
+                <p>payment: {payment}</p>
+
+                <label>
+                    <input  type="radio" value="Pick Up"
+                            checked={shipping === "Pick Up"}
+                            onChange={handleShippingChange}/>
+                    pick up
+                </label>
+                <br/>
+                <label>
+                    <input  type="radio" value="Delivery"
+                                checked={shipping === "Delivery"}
+                                onChange={handleShippingChange}/>
+                    Delivery
+                </label>
+                <p>Shipping: {shipping}</p>
             </div>
 
     );
